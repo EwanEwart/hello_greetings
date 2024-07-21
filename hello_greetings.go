44933979e11c143a024e9b778d23f7fcc49cace9
EwanEwart <EwanEwart@HotMail.Co.UK>
2024-07-21 16:30:28 +0100

    added comments

--- hello_greetings.go
+++ hello_greetings.go
@@ -12,19 +12,30 @@ import (
 
 func main() {
 	/* Set properties of the predefined Logger,
-	including the log entry prefix
-	and a flag to disable printing the time, source file, and line number.
+	including the "log entry prefix"
+	and a "flag" to disable printing the time, source file, and line number.
 	*/
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
 	// request a greetings message
+	// Assign both of the Hello return values, including the error, to variables
+	// Change the Hello argument from a specific name to an empty string, so you can try out the error-handling code.
+	// message, err := greetings.Hello("Kate")
 	message, err := greetings.Hello("")
 	/*
 		If an error was returned,
 		print it to the console and exit the program.
+		Look for a non-nil error value. There's no sense continuing in this case.
 	*/
 	if err != nil {
+		/*
+			Use the functions in the standard library's "log package"
+			to output error information.
+			If you receive an error,
+			you use "the log package's "Fatal" function"
+			to print the error and stop the program.
+		*/
 		log.Fatal(err)
 	}
 	/*
